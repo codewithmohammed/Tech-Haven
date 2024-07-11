ab619ad1e73b2fb4a613a06f6642913a9dceac1e
*Mohammed <rayidrasal@gmail.com>
2024-07-11 14:03:17 +0530

    scaffold color is changed

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -5,54 +5,55 @@ import 'package:tech_haven/core/theme/app_pallete.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-    fontFamily: 'BeVietnamPro',
+      fontFamily: 'BeVietnamPro',
 
-    // brightness: Brightness.light,
-    primaryColor: AppPallete.primaryAppColor,
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: AppPallete.primaryAppButtonColor),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor:
-            WidgetStateProperty.all<Color>(AppPallete.primaryAppButtonColor),
+      // brightness: Brightness.light,
+      primaryColor: AppPallete.primaryAppColor,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: AppPallete.primaryAppButtonColor),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor:
+              WidgetStateProperty.all<Color>(AppPallete.primaryAppButtonColor),
+        ),
       ),
-    ),
+      scaffoldBackgroundColor: AppPallete.backgroundColor
 
-    // bottomSheetTheme: const BottomSheetThemeData(
-    //   showDragHandle: true,
-    //   elevation: 0,
-    //   modalElevation: 0,
-    //   backgroundColor: Colors.transparent,
-    //   modalBackgroundColor: Colors.transparent,
-    //   dragHandleSize: Size(
-    //     double.maxFinite,
-    //     20,
-    //   ),
-    //   // constraints: BoxConstraints(maxHeight: 30, minHeight: 20),
-    //   shape: BeveledRectangleBorder(
-    //     borderRadius: BorderRadius.all(
-    //       Radius.circular(
-    //         0,
-    //       ),
-    //     ),
-    //   ),
-    // ),
-    // iconButtonTheme: IconButtonThemeData(
-    //   style: ButtonStyle(
-    //     backgroundColor: MaterialStateProperty.all<Color>(
-    //         AppPallete.primaryAppButtonColor),
-    //   ),
-    // )
-    // inputDecorationTheme: InputDecorationTheme(
-    //   border: OutlineInputBorder(
-    //       borderRadius: BorderRadius.circular(20.0),
-    //       borderSide: BorderSide.none),
-    //   filled: true,
-    //   fillColor: Colors.grey.withOpacity(
-    //     0.1,
-    //   ),
-    // ),
-  );
+      // bottomSheetTheme: const BottomSheetThemeData(
+      //   showDragHandle: true,
+      //   elevation: 0,
+      //   modalElevation: 0,
+      //   backgroundColor: Colors.transparent,
+      //   modalBackgroundColor: Colors.transparent,
+      //   dragHandleSize: Size(
+      //     double.maxFinite,
+      //     20,
+      //   ),
+      //   // constraints: BoxConstraints(maxHeight: 30, minHeight: 20),
+      //   shape: BeveledRectangleBorder(
+      //     borderRadius: BorderRadius.all(
+      //       Radius.circular(
+      //         0,
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      // iconButtonTheme: IconButtonThemeData(
+      //   style: ButtonStyle(
+      //     backgroundColor: MaterialStateProperty.all<Color>(
+      //         AppPallete.primaryAppButtonColor),
+      //   ),
+      // )
+      // inputDecorationTheme: InputDecorationTheme(
+      //   border: OutlineInputBorder(
+      //       borderRadius: BorderRadius.circular(20.0),
+      //       borderSide: BorderSide.none),
+      //   filled: true,
+      //   fillColor: Colors.grey.withOpacity(
+      //     0.1,
+      //   ),
+      // ),
+      );
 
   static var inputDecoration = InputDecoration(
     filled: true,
@@ -91,12 +92,14 @@ class AppTheme {
               WidgetStateProperty.all<Color>(AppPallete.primaryAppButtonColor),
         ),
       ),
+      scaffoldBackgroundColor: AppPallete.backgroundColor,
       iconButtonTheme: IconButtonThemeData(
         style: ButtonStyle(
           backgroundColor:
               WidgetStateProperty.all<Color>(AppPallete.primaryAppButtonColor),
         ),
       )
+
       // inputDecorationTheme: InputDecorationTheme(
       //   border: OutlineInputBorder(
       //       borderRadius: BorderRadius.circular(20.0),
